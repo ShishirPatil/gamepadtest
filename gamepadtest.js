e7e0f0a15ea85380ee85cb8256a8609040e95c3c
Shishir Patil <shishirpatil@berkeley.edu>
2020-08-19 16:32:56 -0700

    Working for Xbox One and PS4 DS controllers

--- gamepadtest.js
+++ gamepadtest.js
@@ -1,10 +1,14 @@
 /*
- * Gamepad API Test
- * Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
+ * Original:
+ *  Gamepad API Test
+ *  
+ *  Written in 2013 by Ted Mielczarek <ted@mielczarek.org>
  *
- * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ *  To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
  *
- * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *  You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ *
+ * Forked by ShishirPatil from https://github.com/luser/gamepadtest on Aug 12, 2020
  */
 var haveEvents = 'GamepadEvent' in window;
 var haveWebkitEvents = 'WebKitGamepadEvent' in window;
@@ -17,27 +21,50 @@ function connecthandler(e) {
   addgamepad(e.gamepad);
 }
 function addgamepad(gamepad) {
-  var keys = new Array("X", "O", "[]", "/\\", "L1", "R1", "L2", "R2", "Sh", "Op", "L3", "R3", "DU", "DD", "DL", "DR", "PS", "Pad");
+  var ps4_keys = new Array("X", "O", "[]", "/\\", "L1", "R1", "L2", "R2", "Sh", "Op", "L3", "R3", "DU", "DD", "DL", "DR", "PS", "Pad");
+  var xbox_keys = new Array("A", "B", "X", "Y", "LB", "RB", "Ba", "St", "L3", "R3", "LT", "RT", "X", 14, 15, 16, 17, 18);
+  var keys_list = new Array(ps4_keys, xbox_keys);
   var axes_names = new Array("LX", "LY", "RX", "RY")
   var gamepadType = new Array("Play Station DS", "XBOX One")
+  var defaultgamePadType = 0
+  var keys = keys_list[defaultgamePadType];
+  // Get controller
   controllers[gamepad.index] = gamepad; var d = document.createElement("div");
   d.setAttribute("id", "controller" + gamepad.index);
   var t = document.createElement("h1");
-  t.appendChild(document.createTextNode("gamepad: " + gamepad.id));
+  t.appendChild(document.createTextNode("Gamepad name: " + gamepad.id));
   d.appendChild(t);
-
+  // Toggle gamepad type
+  var button = document.createElement("input");
+  button.type = "button";
+  button.value = "Toggle GamePad Type";
+  button.onclick = toggleGamepadType;
+  d.appendChild(button);
+  linebreak = document.createElement("br");
+  d.appendChild(linebreak);
+  // Display type of controller
   var gt = document.createElement("div");
-  gt.className = "gamepadType"
-  gt.appendChild(document.createTextNode("GamePad type: " + gamepadType[0]));
+  gt.id = 'toggle';
+  gt.innerHTML = '<h2> Type: '+ gamepadType[defaultgamePadType] +' </h2>'
   d.appendChild(gt);
   
-
+  // Update display on toggle
+  function toggleGamepadType(){
+    defaultgamePadType = 1 - defaultgamePadType;
+    document.getElementById("toggle").innerHTML = '<h2> Type: '+ gamepadType[defaultgamePadType] +' </h2>';
+    keys = keys_list[defaultgamePadType]
+    for (var i=0; i<gamepad.buttons.length; i++) {
+        e.id = "b" + i;
+        document.getElementById(e.id).innerHTML = keys[i];
+      }
+  } 
+  // Create the layout
   var b = document.createElement("div");
   b.className = "buttons";
   for (var i=0; i<gamepad.buttons.length; i++) {
     var e = document.createElement("span");
     e.className = "button";
-    //e.id = "b" + i;
+    e.id = "b" + i;
     e.innerHTML = keys[i];
     b.appendChild(e);
   }
@@ -47,7 +74,7 @@ function addgamepad(gamepad) {
   for (i=0; i<gamepad.axes.length; i++) {
     e = document.createElement("meter");
     e.className = "axis";
-    //e.id = "a" + i;
+    e.id = "a" + i;
     e.setAttribute("min", "-1");
     e.setAttribute("max", "1");
     e.setAttribute("value", "0");
@@ -55,6 +82,15 @@ function addgamepad(gamepad) {
     a.appendChild(e);
   }
   d.appendChild(a);
+  
+  // Credits
+  var cred = document.createElement("div");
+  var t1 = document.createElement("p");
+  t1.innerHTML = '<p> \r\n By Shishir. Forked from <a href="https://github.com/luser">@luser</a></p>';
+  cred.append(t1);
+  d.appendChild(cred)
+
+  // Ending stuff
   document.getElementById("start").style.display = "none";
   document.body.appendChild(d);
   rAF(updateStatus);
